36bbecd93630c7dda17dab39de792b3d07d73e72
sainithin-palli <sainithinpalli22102002@gmail.com>
2025-04-19 17:41:05 +0530

    UserController.java added

--- src/main/java/com/sai/spring_sec_demo/controller/UserController.java
+++ src/main/java/com/sai/spring_sec_demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.sai.spring_sec_demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,14 @@ import com.sai.spring_sec_demo.service.UserService;
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12); // number of rounds we have to go with 12
 
     @PostMapping("register")
+
     public User register(@RequestBody User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        System.out.println("The new Hased Password is: "+user.getPassword());
         return userService.saveUser(user);
     }
 
